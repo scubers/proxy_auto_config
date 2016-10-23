e4afda77333ea95518636d4dffe62016f246c752
王俊仁 <jr-wong@qq.com>
2016-10-23 17:57:32 +0800

    create

---
+++ server.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+/// <reference path="./typings/tsd.d.ts" />
+
+const http = require("http")
+const fs = require("fs")
+
+function getParameters(url) {
+    var parameters = {}
+
+    var paramstr = url.match(/\?.*$/) + ""
+    paramstr = paramstr.substring(1, paramstr.length)
+    console.log(paramstr)
+    var arr = paramstr.split("&")
+    for (var i in arr) {
+        var keyvalue = arr[i].split("=")
+        parameters[keyvalue[0]] = keyvalue[1]
+    }
+
+    return parameters
+}
+
+function getHostIp() {
+    const os = require("os")
+    var a = os.networkInterfaces()
+    var b = a["en0"]
+    for (var i in b) {
+        if (b[i].family == "IPv4") {
+            console.log(b[i].address)
+            return b[i].address
+        }
+    }
+}
+
+const server = http.createServer((req, resp) => {
+
+    var parameters = getParameters(req.url)
+
+    var a = parameters["a"]
+    var b = parameters["b"]
+
+    var temp = fs.readFileSync("pac.js", "utf-8")
+    temp = temp.replace("[template]", getHostIp())
+
+
+    resp.write(temp)
+    resp.end()
+
+})
+
+server.listen(8990)
---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="rx/rx-lite.d.ts" />
+/// <reference path="rx/rx.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="form-data/form-data.d.ts" />
+/// <reference path="request/request.d.ts" />
